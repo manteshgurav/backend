926dbda2953aaf98831ea47269051be84cde674c
manteshgurav <158148048+manteshgurav@users.noreply.github.com>
2024-05-22 17:32:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server.js
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -12,7 +10,7 @@ mongoose.set("strictQuery", false);
 // Connect to MongoDB Cloud database
 mongoose
   .connect(
-    "mongodb+srv://user2000:user3000@test-pro-db.wu36v74.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://user2000:user3000@test-pro-db.wu36v74.mongodb.net/employeeDetails?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to MongoDB");
@@ -24,29 +22,134 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-// Define a Mongoose model for your data
-const Item = mongoose.model("Item", {
-  name: String,
-  description: String,
-
-  // Add other fields as needed
+const Employee = mongoose.model("Employee", {
+  employeeName: String,
+  address: String,
+  phoneNumber: Number,
+  employeeType: String,
+  salaryFrom: Number,
+  salaryTo: Number,
 });
 
-// API endpoint to get all items
-app.get("/api/items", async (req, res) => {
+const SiteDetail = mongoose.model("SiteDetail", {
+  locationName: String,
+  address: String,
+  startDate: Date,
+  targetDate: Date,
+});
+
+const AddEmployeeToSite = mongoose.model("AddEmployeeToSite", {
+  siteId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SiteDetail",
+    required: true,
+  },
+  employeeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Employee",
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+// API endpoint to get all Employee items
+app.get("/api/employeeDetails", async (req, res) => {
   try {
-    const items = await Item.find();
-    res.json(items);
+    const employeeDetails = await Employee.find();
+    res.json(employeeDetails);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// API endpoint to create a new item
-app.post("/api/items", async (req, res) => {
+// API endpoint to get all SiteDetail items
+app.get("/api/siteDetails", async (req, res) => {
   try {
-    const newItem = new Item(req.body);
+    const siteDetails = await SiteDetail.find();
+    res.json(siteDetails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Endpoint to get a SiteDetail by ID
+app.get("/api/siteDetails/:id", async (req, res) => {
+  try {
+    const siteDetail = await SiteDetail.findById(req.params.id);
+
+    if (!siteDetail) {
+      return res.status(404).json({ error: "SiteDetail not found" });
+    }
+
+    res.json(siteDetail);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// API endpoint to delete an Employee by ID
+app.delete("/api/employeeDetails/:id", async (req, res) => {
+  try {
+    const deletedItem = await Employee.findOneAndDelete({ _id: req.params.id });
+
+    if (!deletedItem) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+
+    res.json(deletedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// API endpoint to delete a SiteDetail by ID
+app.delete("/api/siteDetails/:id", async (req, res) => {
+  try {
+    const deletedItem = await SiteDetail.findOneAndDelete({
+      _id: req.params.id,
+    });
+
+    if (!deletedItem) {
+      return res.status(404).json({ error: "SiteDetail not found" });
+    }
+
+    res.json(deletedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// API endpoint to create a new Employee
+app.post("/api/employeeDetails", async (req, res) => {
+  try {
+    const {
+      employeeName,
+      address,
+      phoneNumber,
+      employeeType,
+      salaryFrom,
+      salaryTo,
+    } = req.body;
+    const newItem = new Employee({
+      employeeName,
+      address,
+      phoneNumber,
+      employeeType,
+      salaryFrom,
+      salaryTo,
+    });
     await newItem.save();
     res.json(newItem);
   } catch (error) {
@@ -55,12 +158,47 @@ app.post("/api/items", async (req, res) => {
   }
 });
 
-// API endpoint to update an item by ID
-app.put("/api/items/:id", async (req, res) => {
+// API endpoint to create a new SiteDetail
+app.post("/api/siteDetails", async (req, res) => {
   try {
-    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
+    const { locationName, address, startDate, targetDate } = req.body;
+    const newItem = new SiteDetail({
+      locationName,
+      address,
+      startDate,
+      targetDate,
     });
+    await newItem.save();
+    res.json(newItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// API endpoint to update an Employee by ID
+app.put("/api/employeeDetails/:id", async (req, res) => {
+  try {
+    const {
+      employeeName,
+      address,
+      phoneNumber,
+      employeeType,
+      salaryFrom,
+      salaryTo,
+    } = req.body;
+    const updatedItem = await Employee.findByIdAndUpdate(
+      req.params.id,
+      {
+        employeeName,
+        address,
+        phoneNumber,
+        employeeType,
+        salaryFrom,
+        salaryTo,
+      },
+      { new: true }
+    );
     res.json(updatedItem);
   } catch (error) {
     console.error(error);
@@ -68,26 +206,66 @@ app.put("/api/items/:id", async (req, res) => {
   }
 });
 
-app.post("/api/login", (req, res) => {
-  const { username, password } = req.body;
-
-  if (username === "Mashu" && password === "12345") {
-    res.json({ success: true });
-  } else {
-    res.status(401).json({ success: false, message: "Invalid credentials" });
+// API endpoint to update a SiteDetail by ID
+app.put("/api/siteDetails/:id", async (req, res) => {
+  try {
+    const { locationName, address, startDate, targetDate } = req.body;
+    const updatedItem = await SiteDetail.findByIdAndUpdate(
+      req.params.id,
+      { locationName, address, startDate, targetDate },
+      { new: true }
+    );
+    res.json(updatedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// API endpoint to delete an item by ID
-app.delete("/api/items/:id", async (req, res) => {
+// API endpoint to create an AddEmployeeToSite record
+app.post("/api/addEmployeeToSite", async (req, res) => {
   try {
-    const deletedItem = await Item.findOneAndDelete({ _id: req.params.id });
+    const { siteId, employeeId, startDate, endDate } = req.body;
+    const newRecord = new AddEmployeeToSite({
+      siteId,
+      employeeId,
+      startDate,
+      endDate,
+    });
+    await newRecord.save();
+    res.json(newRecord);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-    if (!deletedItem) {
-      return res.status(404).json({ error: "User not found" });
+// API endpoint to update an AddEmployeeToSite record
+app.put("/api/addEmployeeToSite/:id", async (req, res) => {
+  try {
+    const { siteId, employeeId, startDate, endDate } = req.body;
+    const updatedRecord = await AddEmployeeToSite.findByIdAndUpdate(
+      req.params.id,
+      { siteId, employeeId, startDate, endDate },
+      { new: true }
+    );
+    if (!updatedRecord) {
+      return res.status(404).json({ error: "Record not found" });
     }
+    res.json(updatedRecord);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-    res.json(deletedItem);
+// API endpoint to get all AddEmployeeToSite records
+app.get("/api/addEmployeeToSite", async (req, res) => {
+  try {
+    const records = await AddEmployeeToSite.find()
+      .populate("siteId")
+      .populate("employeeId");
+    res.json(records);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
